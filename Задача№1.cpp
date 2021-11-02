1b594740c4a85df69228fb08903e1f87be2089e1
ilyinNikolay <92156926+ilyinNikolay@users.noreply.github.com>
2021-11-02 12:30:11 +0300

    Update Задача№1.cpp

--- Задача№1.cpp
+++ Задача№1.cpp
@@ -19,7 +19,7 @@ int main()
     c = x / 10;
     while (f == false)
     {
-        if ((100 < d && d > 999) || ((a == b) || (b == c) || (a == c)))
+        if ((100 > d || d > 999) || ((a == b) || (b == c) || (a == c)))
         {
             cout << "Введите трехзначное число еще раз: ";
             cin >> x;
@@ -29,7 +29,7 @@ int main()
             b = x % 10;
             c = x / 10;
         }
-        else 
+        else
         {
             f = true;
         }
@@ -41,24 +41,24 @@ int main()
     cout << " " << b << c << a << endl;
     cout << " " << c << a << b << endl;
     cout << " " << c << b << a << endl;
-   
-    if ((a > b) && (a > c)) 
+
+    if ((a > b) && (a > c))
     {
-    if (b > c)
-        cout << "Самое большое число: " << a << b << c;
-    else
-        cout << "Самое большое число: " << a << c << b;
-    }   
+        if (b > c)
+            cout << "Самое большое число: " << a << b << c;
+        else
+            cout << "Самое большое число: " << a << c << b;
+    }
     else
     {
-        if ((b > a) && (b > c)) 
+        if ((b > a) && (b > c))
         {
             if (a > c)
                 cout << "Самое большое число: " << b << a << c;
             else
                 cout << "Самое большое число: " << b << c << a;
         }
-        else 
+        else
         {
             if ((c > a) && (c > b))
             {
