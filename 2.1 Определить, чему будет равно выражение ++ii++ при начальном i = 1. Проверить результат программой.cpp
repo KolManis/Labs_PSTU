be987fbedeaa4a63d40d823497b68ab43d628f65
ilyinNikolay <92156926+ilyinNikolay@users.noreply.github.com>
2021-11-03 18:07:00 +0300

    Delete 2.1 Определить, чему будет равно выражение ++ii++ при начальном i = 1. Проверить результат программой.cpp

--- 2.1 Определить, чему будет равно выражение ++ii++ при начальном i = 1. Проверить результат программой.cpp
+++
@@ -1,9 +0,0 @@
-// 2.1 Определить, чему будет равно выражение ++i*i++ при начальном i = 1. Проверить результат программой.
-#include <iostream>
-using namespace std;
-int i = 1;
-int main() 
-{
-	cout << ++i * i++;
-	return 0;
-}
